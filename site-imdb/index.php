28b180b26a7cee2f92c35bdf937ddbc98874ed60
Tadeu Pagliarini <tad3eu@gmail.com>
2015-11-03 16:39:16 -0200

    operacoes e variaveis

--- site-imdb/index.php
+++ site-imdb/index.php
@@ -5,7 +5,24 @@ require 'vendor/autoload.php';
 $app = new \Slim\Slim();
 
 $app->get('/hello/:name', function ($name) {
-    echo "Hello, " . $name;
+    $inteiro1 = 42;
+    $inteiro2 = 79;
+    $texto = "Ola, ";
+    $fracao = 2.3;
+    
+    $soma = $inteiro1 + $inteiro2;
+    $subtracao = $inteiro2 - $inteiro1;
+    $multi = $inteiro1 * $fracao;
+    $divisao = $inteiro2 / $fracao;
+    
+    $olaMundo = $texto . $name;
+    
+    echo "<p>Soma: " . $soma . "</p>";
+    echo "<p>Subtracao: " . $subtracao . "</p>";
+    echo "<p>Multiplicacao: " . $multi . "</p>";
+    echo "<p>Divisao: " . $divisao . "</p>";
+    
+    echo "<p>" . $olaMundo . "</p>";
 });
 
 $app->run();
